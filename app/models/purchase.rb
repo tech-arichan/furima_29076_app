4ae2940914ef57a6f54f33dcc6b297a6d292381e
tech-arichan <arihiro2940@gmail.com>
2021-02-04 23:32:20 +0900

    purchaseモデル作成　payjp導入

---
+++ app/models/purchase.rb
@@ -0,0 +1,19 @@
+class Purchase
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :token
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :city
+    validates :house_number
+    validates :phone_number, format: {with: /\A\d{10,11}\z/}
+    validates :token
+  end
+
+
+  def save
+    item_purchase = ItemPurchase.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, item_purchase_id: item_purchase.id)
+  end
+end
