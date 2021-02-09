c334c8c7d6152603bacbf20efc3475e1d5805f29
tech-arichan <arihiro2940@gmail.com>
2021-02-09 11:41:49 +0900

    Capfile導入、「production.rb」編集、「deploy.rb」編集、「unicorn.rb」編集

---
+++ config/deploy.rb
@@ -0,0 +1,29 @@
+lock '3.15.0'
+
+set :application, 'furima_29076_app'
+
+set :repo_url,  'git@github.com:
+tech-arichan/furima_29076_app.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5'
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/furimaApp.pem'] 
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
